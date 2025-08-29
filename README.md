b8cf0baa1392cad4f04b436fc2f991f99e3d0e11
Tetsuya <1376490336@qq.com>
2025-08-29 19:57:29 +0800

    feat: refact instruct

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Axiom
 
 Declarative NixOS dotfiles that treat every boot as a self-evident, reproducible axiom.
+
+
+## Q&A
+
+**Q: How to manage dotfiles?**
+**A**: Use home-manager. You can execute the command `nix run home-manager/master -- switch --flake .#syourl`, this command will temporary execution home-manager and switch the config which is defined in the flake.nix.
+
+
+## Reference
+* [Home Manager](https://github.com/nix-community/home-manager)
+* [NixOS Wiki - Home Manager](https://nixos.wiki/wiki/Home_Manager)
+* [mitchellh - nixos-config](https://github.com/mitchellh/nixos-config/blob/main/users/mitchellh/home-manager.nix)
--- flake.nix
+++ flake.nix
@@ -1,25 +1,36 @@
 {
-  description = "Bowen Tu's Flake File";
+  description = "My NixOS/flake config with home-manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+ outputs = { self, nixpkgs, home-manager }:
     let
-      system = "aarch64-darwin";
+      system = "aarch64-darwin";  # 根据你的系统修改
       pkgs = nixpkgs.legacyPackages.${system};
+      username = "Bowen Tu";
     in
-  {
-      packages.${system}.default = pkgs.hello;
-      
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ pkgs.hello ];
+    {
+      # 💡 定义你的 home-manager 配置
+      homeConfigurations.syourl = home-manager.lib.homeConfiguration {
+        username = username;
+        homeDirectory = "/home/${username}";
+        configuration = {
+          imports = [ ./home.nix ];
+        };
       };
-  };
+
+      # ✅ 关键：定义 devShell，让 nix develop 可用
+      devShells.${system}.default = pkgs.mkShell {
+        # 把 home-manager 命令加进来
+        buildInputs = [
+          home-manager.packages.${system}.home-manager
+        ];
+      };
+    };
 }
